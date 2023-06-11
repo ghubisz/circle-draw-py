d41747548d50a56041e20eebd44088932bf8a94e
ghubisz <gab.op.pl@op.pl>
2023-06-11 23:01:36 +0200

    3/4

---
+++ small-circle.py
@@ -0,0 +1,7 @@
+import turtle
+
+t = turtle.Turtle()
+
+r = 50
+t.circle(r)
+
---
+++ spiral-circle.py
@@ -0,0 +1,11 @@
+import turtle
+
+t = turtle.Turtle()
+
+#taking radius of initial radius
+r = 10
+
+#loop for printing spiral circle
+
+for i in range(100):
+    t.circle(r +i, 45)
